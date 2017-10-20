7813d1f688274363609efb2c63e0a11f5182ffc9
Svetoslav Todorov <drnerf95@gmail.com>
2017-10-20 12:30:25 +0300

    mada mada

--- XgagSPA/ClientApp/components/Home.tsx
+++ XgagSPA/ClientApp/components/Home.tsx
@@ -3,23 +3,77 @@ import { RouteComponentProps } from 'react-router';
 import { RuntimeInfo } from '../RuntimeInfo'
 import { PostsProxy } from '../Proxies/PostsProxy'
 import { ProgressBar } from 'react-bootstrap'
+import * as Models from '../Proxies/ProxyModels'
+import { Post } from './Posts/Post'
+import { Spinner } from './System/spinner'
+//import { Reac } from 'react-addons-css-transition-group'
 
-export class Home extends React.Component<RouteComponentProps<{}>, {}> {
+interface HomeState {
+    posts: Models.PostModel[];
+}
+
+export class Home extends React.Component<RouteComponentProps<{}>, HomeState> {
     private postsProxy = new PostsProxy();
 
     constructor() {
         super();
+        this.state = {
+            posts: []
+        };
     }
 
     public render() {
         return <div className='container'>
+            <br />
             <div className='col-lg-8'>
-                <button className='btn btn-default' onClick={async () => { console.log(await this.postsProxy.getPosts(1)); }}>
-                    Test
-                </button>
+                {this.state.posts.map(post =>
+                    <div className='row' key={post.postId}>
+                        <Post {...post} />
+                    </div>)}
+                <div className='row text-center'>
+                    <Spinner width={50} height={50} />
+                </div>
+                <br />
             </div>
             <div className='col-lg-4'>
             </div>
         </div>;
+    }
+
+    public componentDidMount() {
+        window.addEventListener('scroll', () => this.handleOnScroll());
+
+        this.pushDemoPost();
+    }
+
+    public componentWillUnmount() {
+        window.removeEventListener('scroll', () => this.handleOnScroll());
+    }
+
+    private handleOnScroll() {
+        var scrollTop = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+        var scrollHeight = (document.documentElement && document.documentElement.scrollHeight) || document.body.scrollHeight;
+        var clientHeight = document.documentElement.clientHeight || window.innerHeight;
+        var scrolledToBottom = Math.ceil(scrollTop + clientHeight) >= scrollHeight;
+
+        if (scrolledToBottom) {
+            setTimeout(() => this.pushDemoPost(), 2000);
+        }
+    }
+
+    private demoIdCounter = 1;
+    private pushDemoPost(): void {
+        var newState = this.state.posts.concat([{
+            postId: this.demoIdCounter,
+            title: 'Demo',
+            dateCreatedTicks: 1231241234,
+            imageUrl: 'https://img-9gag-fun.9cache.com/photo/aB8Xq1N_700b.jpg',
+            youTubeLink: '',
+            isNsfw: false,
+            isYoutubePost: false,
+            score: 12
+        }]);
+        this.setState({ posts: newState })
+        this.demoIdCounter++;
     }
 }
---
+++ XgagSPA/ClientApp/components/Posts/Post.tsx
@@ -0,0 +1,11 @@
+﻿import * as React from 'react'
+import { Panel } from 'react-bootstrap'
+import * as Models from '../../Proxies/ProxyModels'
+
+export class Post extends React.Component<Models.PostModel, {}>{
+    public render() {
+        return <Panel header={this.props.title} footer='Vote buttons and score here!'>
+            <img className='post-image' src={this.props.imageUrl} />
+        </Panel>;
+    }
+}
